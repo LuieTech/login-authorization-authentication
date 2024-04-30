051c634e55e01d34efb4c2498c6b458030a4bb2d
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-04-30 18:33:02 -0400

    chore:"implement route for signup page"

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom"
 import LoginPage from './pages/login/LoginPage'
 import NavBar from './components/NavBar'
 import TaskList from './pages/tasks/list'
+import SignUpPage from './pages/signup/SignUpPage'
 
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/tasks" element={<TaskList />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+
       </Routes>
     </>
   )
