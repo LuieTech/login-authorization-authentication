240fce7abf85b8b002c25ff5b917e2505ac2536e
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-05-05 15:08:25 -0400

    chore:"implement showing users email importing context component "

--- web/src/components/NavBar.jsx
+++ web/src/components/NavBar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useAuthContext } from '../contexts/auth-context'
 
 function NavBar() {
+
+  const value = useAuthContext()
+
   return (
-    <nav className='p-4 text-secondary'>React Task Manager</nav>
+    <nav className='p-4 text-secondary'>
+      <div className='container-fluid'>
+        <span className='navbar-brand mb-0 h1'>
+          <strong>React Task Manager | {value.user?.email}</strong>
+        </span>
+      </div>
+    </nav>
   )
 }
 
