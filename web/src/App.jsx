c97eba81b3d27ff5fa76e046d791067770d6301d
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-05-05 15:07:49 -0400

    chore:"Implement a context component for managing users authentication"

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,20 +1,31 @@
 import { useState } from 'react'
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Navigate, useLocation} from "react-router-dom"
 import LoginPage from './pages/login/LoginPage'
 import NavBar from './components/NavBar'
 import TaskList from './pages/tasks/list'
 import SignUpPage from './pages/signup/SignUpPage'
-
+import TaskDetail from './pages/tasks/detail'
+import { useAuthContext } from './contexts/auth-context'
 
 function App() {
+  // const { user } = useAuthContext();
+  // const location = useLocation();
+
+  // function AuthRoute(props){ this doesn't work cause it needs to be Route or React.Fragment
+  //   return <Route {...props} />
+  // }
+  
+  // if(!user && location.pathname !== "/login") return <Navigate to="/login" />
 
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/tasks" element={<TaskList />} />
+        <Route path="/login" element={<LoginPage />} /> 
         <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/tasks/:id" element={<TaskDetail />} />
+        <Route path='*' element={<Navigate to="/tasks" />} />
 
       </Routes>
     </>
