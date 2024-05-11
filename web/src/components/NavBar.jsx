c07b21023fb19ca2ae89f503b45e729a1bb0b4ad
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-05-11 15:55:09 -0400

    chore:"implement the iconify/react libary in the Navbar component"

--- web/src/components/NavBar.jsx
+++ web/src/components/NavBar.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import {Icon} from '@iconify/react'
 import { useAuthContext } from '../contexts/auth-context'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
 
-  const value = useAuthContext()
+  const {user, onLogout} = useAuthContext()
 
   return (
-    <nav className='p-4 text-secondary'>
+    <nav className='navbar bg-body-tertiary'>
       <div className='container-fluid'>
         <span className='navbar-brand mb-0 h1'>
-          <strong>React Task Manager | {value.user?.email}</strong>
+          <Link to="/" className='text-reset text-decoration-none'>
+            <strong>React Task Manager | </strong>
+          </Link>
+            <span className='small'>{user?.email}</span>            
         </span>
+        { user && 
+          <button className='btn btn-sm btn-danger d-flex align-items-center' onClick={() => onLogout()} ><Icon icon="uiw:logout" className='me-1'/>Logout</button>
+        }
       </div>
     </nav>
   )
