8f32085a85ef7116eef88a7883e7c64438320dea
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-05-11 15:54:26 -0400

    chore:"implement an Authorization Route component to authorize authenticated users and added it in some frontend routes"

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,30 +1,30 @@
 import { useState } from 'react'
-import {Routes, Route, Navigate, useLocation} from "react-router-dom"
+import {Routes, Route, Navigate } from "react-router-dom"
 import LoginPage from './pages/login/LoginPage'
 import NavBar from './components/NavBar'
 import TaskList from './pages/tasks/list'
 import SignUpPage from './pages/signup/SignUpPage'
 import TaskDetail from './pages/tasks/detail'
-import { useAuthContext } from './contexts/auth-context'
+import { Authenticated, OpenRoute } from './components/Authentication/Authenticated'
 
 function App() {
-  // const { user } = useAuthContext();
-  // const location = useLocation();
-
-  // function AuthRoute(props){ this doesn't work cause it needs to be Route or React.Fragment
-  //   return <Route {...props} />
-  // }
-  
-  // if(!user && location.pathname !== "/login") return <Navigate to="/login" />
 
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="/tasks" element={<TaskList />} />
-        <Route path="/login" element={<LoginPage />} /> 
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/tasks/:id" element={<TaskDetail />} />
+        <Route path="/tasks" element={ 
+           <Authenticated><TaskList /></Authenticated>
+          } />
+        <Route path="/login" element={
+          <OpenRoute><LoginPage /></OpenRoute> 
+        } /> 
+        <Route path="/register" element={
+          <OpenRoute><SignUpPage /></OpenRoute> 
+          } />
+        <Route path="/tasks/:id" element={
+          <Authenticated><TaskDetail /></Authenticated> 
+          } />
         <Route path='*' element={<Navigate to="/tasks" />} />
 
       </Routes>
