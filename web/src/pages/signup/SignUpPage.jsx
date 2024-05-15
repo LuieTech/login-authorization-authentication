ed5342ebef03a828540d9e5ab39cb70f506cf433
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-05-15 11:53:47 -0400

    chore:"implement name and image fields in the registration page "

--- web/src/pages/signup/SignUpPage.jsx
+++ web/src/pages/signup/SignUpPage.jsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form'
-import { Navigate, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { login, registerUser } from '../../services/users-service'
 import { useAuthContext } from '../../contexts/auth-context'
 
@@ -25,12 +25,20 @@ function handleSignup(formData){
     <h1>Register</h1>
     <form onSubmit={handleSubmit(handleSignup)}>
       <div className="mb-3">
+        <label htmlFor="name" className="form-label">Name</label>
+        <input autoComplete='email' type="text" className="form-control" id="name" {...register("name")}/>
+      </div>
+      <div className="mb-3">
         <label htmlFor="email" className="form-label">Email address</label>
         <input autoComplete='email' type="email" className="form-control" id="email" {...register("email")}/>
       </div>
       <div className="mb-3">
         <label htmlFor="password" className="form-label">Password</label>
         <input autoComplete='password' type="password" className="form-control" id="password" {...register("password")}/>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="avatar" className="form-label">Email address</label>
+        <input autoComplete='' type="file" className="form-control" id="avatar" {...register("avatar")}/>
       </div>
       <div className='d-flex gap-3'>
         <button type="submit" className="btn btn-primary rounded-3 ">Send</button>
