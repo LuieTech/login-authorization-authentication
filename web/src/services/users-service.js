7c57fd5acc26f3b4bf8aa59059358da00849999b
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-05-15 11:54:37 -0400

    chore:"modified user service registerUser function to receive user avatar using FormData()"

--- web/src/services/users-service.js
+++ web/src/services/users-service.js
@@ -11,11 +11,19 @@ export function login(data){
 }
 
 export function registerUser(data){
-  return service.post("/users", data)
+
+  const formData = new FormData() 
+
+  formData.append('name', data.name);
+  formData.append('email', data.email);
+  formData.append('password', data.password)
+  if(data.avatar){
+    formData.append('avatar', data.avatar[0])
+  }
+  
+  return service.post("/users", formData)
 }
 
 export function logout(){
   return service.post("/logout").then((response) => response.data)
-// Rest of the code...
-
 }
