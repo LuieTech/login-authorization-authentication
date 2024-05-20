03dc7b4009421d9f3525ac9edce91906feab6d6b
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-05-20 14:00:04 -0400

    chore:"implemert routing for updating"

--- api/config/routes.config.js
+++ api/config/routes.config.js
@@ -15,11 +15,11 @@ router.get('/task-groups/:id', groups.detail)
 
 //Task routes
 router.get('/tasks', auth.isAuthenticated, tasks.list)
-router.get('/tasks/:id', tasks.detail)
+router.get('/tasks/:id', auth.isAuthenticated, tasks.detail)
 router.post('/tasks', tasks.create) // no "/:groupId" when receiving the "groupId" in the body
 // '/task-groups/:groupId/tasks/:id, tasks.create' => including the groupId in the path/url
-router.patch('/tasks', tasks.update)
-// router.delete('/tasks/:id', tasks.delete)
+router.patch('/tasks/:id', tasks.update)
+router.delete('/tasks/:id', auth.isAuthenticated, tasks.delete)
 
 router.post("/users", upload.single('avatar'), users.create)
 router.post("/login", users.login)
